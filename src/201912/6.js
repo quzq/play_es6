47680f37d545e1859465cebebed9846b8295b5c8
quz <quz@dummy.com>
2019-12-06 14:00:25 +0900

    wrapperの引数を、内部関数の引数としてそのまま渡す

--- src/201912/6.js
+++ src/201912/6.js
@@ -1,4 +1,13 @@
 export default () => {
   console.log('--- 20191206.js -----------------------------')
-  console.log('hello!')
+  console.log(`* wrapperの引数を、内部関数の引数としてそのまま渡す`)
+  const wrapper = (...args)=>{
+    console.log(`args.length: ${args.length}`)   //引数の数
+    console.log(`args[0]: ${args[0]}`)  
+    const core = (...args)=>{
+      console.log('core args',args)
+    } 
+    core(...args)
+  }
+  wrapper('hello','world','11') // success
 }
