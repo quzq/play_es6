9ea4ecc6c7e8758c395dfd12542a3a1195748ff4
quzq <quzyam@gmail.com>
2019-12-10 16:49:43 +0900

    add moment-timezone

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import src from "./201912/10"
 import lodashSrc from "./lodash" 
 import momentSrc from "./moment" 
+import momentTzSrc from "./momenttz" 
 
 lodashSrc();
 momentSrc();
+momentTzSrc();
 src();
---
+++ src/momenttz.js
@@ -0,0 +1,17 @@
+import moment from "moment-timezone"
+
+export default ()=>{
+  console.log('--- moment-timezone.js -----------------------------')
+  console.log(`* timezoneを変更しても絶対時間は変わらない`)
+  const now = moment().toDate()
+  console.log(now)
+  console.log(moment.tz(now,'Asia/Tokyo').toDate())
+  console.log(moment(now).utc())
+  console.log(moment(now).utc().toDate())
+  
+  console.log(`* timezoneを指定することで、固定時間の解釈を変えることができる`)
+  const fixed = moment.tz('2019-12-10 16:30:00', 'Asia/Tokyo')
+  console.log(fixed.toDate())
+  const fixed2 = moment.tz('2019-12-10 16:30:00', 'Europe/Stockholm')
+  console.log(fixed2.toDate())
+}
