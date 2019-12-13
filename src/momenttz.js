5dc842572be797bd3e467b62e11ddad1b0cbf343
quzq <quzyam@gmail.com>
2019-12-13 09:16:50 +0900

    unix time has no mili-sec

--- src/momenttz.js
+++ src/momenttz.js
@@ -14,4 +14,11 @@ export default ()=>{
   console.log(fixed.toDate())
   const fixed2 = moment.tz('2019-12-10 16:30:00', 'Europe/Stockholm')
   console.log(fixed2.toDate())
+
+  
+  console.log(`* unix時間にミリ秒はない`)
+  console.log(moment.tz('2019-12-10 16:30:00.134', 'Asia/Tokyo').toDate())
+  console.log(moment.tz('2019-12-10 16:30:01.134', 'Asia/Tokyo').unix())
+  console.log(moment.tz('2019-12-10 16:30:01.000', 'Asia/Tokyo').unix())
+
 }
