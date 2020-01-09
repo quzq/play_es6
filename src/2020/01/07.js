0afcf7176a47b5796f29c8264e64d8d557d0e758
quzq <quzyam@gmail.com>
2020-01-09 14:28:57 +0900

    add favorites

--- src/2020/01/07.js
+++ src/2020/01/07.js
@@ -2,9 +2,16 @@ export default () => {
   console.log('--- 20200107.js -----------------------------')
   console.log('* readonly property ')
   class Foo{
+    get bazz(){return 'bazz is readonly'}
   }  
+  try{
+    foo.bazz= 'changed'
+    console.log(`show %o`, foo.baz)
+  }catch(e){
+    console.log(e)
+  }
   const foo = new Foo()
-  Object.defineProperty(foo, 'bar', {get: ()=> 'const value'})
+  Object.defineProperty(foo, 'bar', {get: ()=> 'bar is readonly'})
   console.log(`show %o`, foo.bar)
   try{
     foo.bar = 'changed'
@@ -12,4 +19,29 @@ export default () => {
   }catch(e){
     console.log(e)
   }
+
+  console.log('* nested readonly property ')
+  class Foobar{
+    get bar() {
+      return {
+        get baz1() { return 'baz1 is readonly' },
+        get baz2() { return 'baz2 is readonly' },
+      }
+    }
+  }  
+  const foobar = new Foobar()
+  try{
+    console.log(`foobar.bar %o`, foobar.bar)
+    foobar.bar = null
+  }catch(e){
+    console.log(e)
+  }
+  try{
+    console.log(`foobar.bar.baz1 %o`, foobar.bar.baz1)
+    foobar.bar.baz1 = null
+  }catch(e){
+    console.log(e)
+  }
+
+
 }
